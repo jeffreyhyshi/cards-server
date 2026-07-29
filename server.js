c1acdb9406ee2ea86ae04b49b2623720f40f7036
jeffreyhyshi <jeffster888@berkeley.edu>
2026-07-29 16:16:47 -0400

    Use 6 character alhpanumeric peer client id

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const { PeerServer } = require("peer");
 
-const server = PeerServer({ port: 9000, path: '/card-peer' });
+const clientIdFunction = () => (Math.random().toString(36)).substr(2, 7);
+
+const server = PeerServer({
+    port: 9000,
+    path: '/card-peer',
+    generateClientId: clientIdFunction
+});
