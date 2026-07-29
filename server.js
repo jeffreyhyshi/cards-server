08cb6acf389c1d5b2219edf7cf3065f0a1621544
jeffreyhyshi <jeffster888@berkeley.edu>
2026-07-29 16:18:37 -0400

    Use 6 characters for real this time

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { PeerServer } = require("peer");
 
-const clientIdFunction = () => (Math.random().toString(36)).substr(2, 7);
+const clientIdFunction = () => (Math.random().toString(36)).substr(2, 6);
 
 const server = PeerServer({
     port: 9000,
